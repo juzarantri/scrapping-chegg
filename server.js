eb08be5871a0cd41c2088feef57c3e9bc29295fb
juzarantri <juzarantri786@gmail.com>
2023-09-30 16:45:16 +0530

    added default route

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const { playTest } = require("./scraper");
 
 const app = express();
 app.use(express.static("./"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(3000, async () => {
   await playTest("https://expert.chegg.com/auth/login");
   console.log("server started at port 3000");
